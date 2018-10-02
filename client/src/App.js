7e360126447290482abe0a17c86a5c953e209acd
Kingluddite <howley.phil@gmail.com>
2018-10-01 22:48:08 -0700

    Make small changes

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+      // some comment
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
