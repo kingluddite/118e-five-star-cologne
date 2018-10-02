ba881f77a135a7c802a79a43cefb3ff6e9f30561
Kingluddite <howley.phil@gmail.com>
2018-10-01 22:34:56 -0700

    Add react and graphql

---
+++ server/models/Cologne.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CologneSchema = new Schema({
+  scentName: {
+    type: String,
+    required: true,
+  },
+  scentPrice: {
+    type: Number,
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now,
+  },
+  description: {
+    type: String,
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+  username: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model('Cologne', CologneSchema);
---
+++ server/schema.js
@@ -0,0 +1,28 @@
+exports.typeDefs = `
+  type Cologne {
+    _id: ID,
+    scentName: String!
+    scentPrice: Int
+    createdDate: String
+    description: String
+    likes: Int
+    username: String
+  }
+
+  type User {
+    _id: ID,
+    username: String!
+    password: String!
+    email: String!
+    joinDate: String
+    favorites: [Cologne]
+  }
+
+  type Query {
+    getAllColognes: [Cologne] 
+  }
+
+  type Mutation {
+    addCologne(scentName: String!, scentPrice: Int, description: String, username: String): Cologne
+  }
+`;
